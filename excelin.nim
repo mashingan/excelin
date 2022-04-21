d1faf5c36c82e288e2a33eeec45a036f6e03d5e1
mashingan <rahmat.d.ruffy@gmail.com>
2022-04-21 20:13:33 +0700

    add documentation comment

--- excelin.nim
+++ excelin.nim
@@ -34,7 +34,6 @@ from zippy/ziparchives import openZipArchive, extractFile, ZipArchive,
 
 const
   datefmt = "yyyy-MM-dd'T'hh:mm:ss'.'fffzz"
-  #excelinVersion = "0.1.0"
   xmlnsx14 = "http://schemas.microsoft.com/office/spreadsheetml/2009/9/main"
   xmlnsr = "http://schemas.openxmlformats.org/officeDocument/2006/relationships"
   xmlnsxdr = "http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing"
@@ -43,9 +42,12 @@ const
   mainns = "http://schemas.openxmlformats.org/spreadsheetml/2006/main"
   relSharedStrScheme = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings"
   emptyxlsx = currentSourcePath.parentDir() / "empty.xlsx"
+  excelinVersion* = "0.1.0"
 
 type
   Excel* = ref object
+    ## The object the represent as Excel file, mostly used for reading the Excel
+    ## and writing it to Zip file and to memory buffer (at later version).
     content: XmlNode
     rels: XmlNode
     workbook: Workbook
@@ -58,18 +60,25 @@ type
     body: XmlNode
 
   Workbook* = ref object of InternalBody
+    ## The object that used for managing package information of Excel file.
+    ## Most users won't need to use this.
     path: string
     sheetsInfo: seq[XmlNode]
     rels: FileRep
     parent: Excel
 
   Sheet* = ref object of InternalBody
+    ## The main object that will be used most of of the time for many users.
+    ## This object will represent a sheet in Excel file such as adding row
+    ## getting row, and/or adding cell directly.
     sharedStrings: XmlNode
     parent: Excel
     rid: string
     privName: string
 
   Row* = ref object of InternalBody
+    ## The object that will be used for working with values within cells of a row.
+    ## Users can get the value within cell and set the its value.
     sheet: Sheet
   FilePath = string
   FileRep = (FilePath, XmlNode)
@@ -77,8 +86,12 @@ type
   ExcelError* = object of CatchableError
 
 template unixSep*(str: string): untyped = str.replace('\\', '/')
+  ## helper to change the Windows path separator to Unix path separator
 
 proc getSheet*(e: Excel, name: string): Sheet =
+  ## Fetch the sheet from the Excel file for further work.
+  ## Will return nil for unavailable sheet name.
+  ## Check with `sheetNames proc<#sheetNames,Excel>`_ to find out which sheets' name available.
   var rid = ""
   for s in e.workbook.body.findAll "sheet":
     if name == s.attr "name":
@@ -103,6 +116,7 @@ proc getSheetData(s: XmlNode): XmlNode =
     result = <>sheetData()
 
 proc modifiedAt*[T: DateTime | Time](e: Excel, t: T = now()) =
+  ## Update Excel modification time.
   const key = "core.xml"
   if key notin e.otherfiles: return
   let (_, prop) = e.otherfiles[key]
@@ -112,9 +126,13 @@ proc modifiedAt*[T: DateTime | Time](e: Excel, t: T = now()) =
   modifn.add newText(t.format datefmt)
 
 proc modifiedAt*[Node: Workbook|Sheet, T: DateTime | Time](w: Node, t: T = now()) =
+  ## Update workbook or worksheet modification time.
   w.parent.modifiedAt t
 
 proc addRow*(s: Sheet): Row =
+  ## Add row directly from sheet after any existing row.
+  ## This will return a new pristine row to work further.
+  ## Row numbering is 1-based.
   let rows = toSeq s.body.findAll("row")
   let newnum = block:
     if rows.len > 0:
@@ -127,6 +145,9 @@ proc addRow*(s: Sheet): Row =
   s.modifiedAt
 
 proc addRow*(s: Sheet, rowNum: Positive): Row =
+  ## Add row by selecting which row number to work with.
+  ## This will return new row if there's no existing row
+  ## or will return an existing one.
   var latestRowNum = 0
   for r in s.body.findAll "row":
     let rnum = try: parseInt(r.attr "r") except: 0
@@ -140,9 +161,12 @@ proc addRow*(s: Sheet, rowNum: Positive): Row =
   s.modifiedAt
 
 proc rowNum*(r: Row): Positive =
+  ## Getting the current row number of Row object users working it.
   result = try: parseInt(r.body.attr "r") except: 1
 
 proc addCell*(row: Row, col: string, s: string) =
+  ## Add cell with overload for value string. Supplied column
+  ## is following the Excel convention starting from A -  Z, AA - AZ ...
   let lastStr = row.sheet.sharedStrings.len
   row.sheet.sharedStrings.add <>si(newXmlTree("t",
     [newText s],
@@ -154,18 +178,49 @@ proc addCell*(row: Row, col: string, s: string) =
   row.sheet.sharedStrings.attrs = {"count": $newcount,
     "uniqueCount": $newcount, "xmlns": mainns}.toXmlAttributes
 proc addCell*(row: Row, col: string, n: SomeNumber) =
+  ## Add cell with overload for any number.
   let cellpos = fmt"""{col}{row.body.attr "r"}"""
   row.body.add <>c(r=cellpos, t="n", <>v(newText $n))
   row.sheet.modifiedAt
 proc addCell*(row: Row, col: string, b: bool) =
+  ## Add cell with overload for truthy value.
   row.body.add <>c(r=fmt"""{col}{row.body.attr "r"}""", t="b", <>v(newText $b))
   row.sheet.modifiedAt
 proc addCell*(row: Row, col: string, d: DateTime | Time) =
+  ## Add cell with overload for DateTime or Time. The saved value
+  ## will be in string format of `yyyy-MM-dd'T'hh:mm:ss'.'fffzz` e.g.
+  ## `2200-10-01T11:22:33.456-03`.
   row.body.add <>c(r=fmt"""{col}{row.body.attr "r"}""", t="d",
-              <>(newText d.format(datefmt)))
+              <>v(newText d.format(datefmt)))
   row.sheet.modifiedAt
 
 proc getCell*[R](row: Row, col: string, conv: string -> R = nil): R =
+  ## Get cell value from row with optional function to convert it.
+  ## When conversion function is supplied, it will be used instead of
+  ## default conversion function viz:
+  ## * SomeSignedInt (int/int8/int32/int64): strutils.parseInt
+  ## * SomeUnsignedInt (uint/uint8/uint32/uint64): strutils.parseUint
+  ## * SomeFloat (float/float32/float64): strutils.parseFloat
+  ## * DateTime | Time: times.format with layout `yyyy-MM-dd'T'hh:mm:ss'.'fffzz`
+  ##
+  ## For simple usage example:
+  ##
+  ## .. code-block:: Nim
+  ##
+  ##   let strval = row.getCell[:string]("A") # we're fetching string value in colum A
+  ##   let intval = row.getCell[:int]("B")
+  ##   let uintval = row.getCell[:uint]("C")
+  ##   let dtval = row.getCell[:DateTime]("D")
+  ##   let timeval = row.getCell[:Time]("E")
+  ##
+  ##   # below example we'll get the DateTime that has been formatted like 2200/12/01
+  ##   # so we supply the optional custom converter function
+  ##   let dtCustom = row.getCell[:DateTime]("F", (s: string) => s.parse("yyyy/MM/dd"))
+  ##
+  ## Any other type that other than mentioned above should provide the closure proc
+  ## for the conversion otherwise it will return the default value, for example
+  ## any ref object will return nil or for object will get the object with its field
+  ## filled with default values.
   let rnum = row.body.attr "r"
   let v = block:
     var x: XmlNode
@@ -210,13 +265,15 @@ proc getCell*[R](row: Row, col: string, conv: string -> R = nil): R =
   else:
     discard
 
+# when adding new sheet, need to update workbook to add
+# ✓ to sheets,
+# ✓ its new id,
+# ✓ its package relationships
+# ✓ add entry to content type
+# ✗ add complete worksheet nodes
 proc addSheet*(e: Excel, name = ""): Sheet =
-  # when adding new sheet, need to update workbook to add
-  # ✓ to sheets,
-  # ✓ its new id,
-  # ✓ its package relationships
-  # ✓ add entry to content type
-  # ✗ add complete worksheet nodes
+  # Add new sheet to excel with supplied name and return it to enable further working.
+  # The name can use the existing sheet name.
   const
     sheetTypeNs = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet"
     contentTypeNs = "application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"
@@ -258,14 +315,17 @@ proc addSheet*(e: Excel, name = ""): Sheet =
   rel[1].add <>Relationship(Target=fpath, Type=sheetTypeNs, Id=rid)
   e.content.add <>Override(PartName="/" & fpath, ContentType=contentTypeNs)
 
+# deleteing sheet needs to delete several related info viz:
+# ✓ deleting from the sheet table
+# ✓ deleting from content
+# ✓ deleting from package relationships
+# ✓ deleting from workbook body
+# ✗ deleting from workbook sheets info
 proc deleteSheet*(e: Excel, name = "") =
-  # deleteing sheet needs to delete several related info viz:
-  # ✓ deleting from the sheet table
-  # ✓ deleting from content
-  # ✓ deleting from package relationships
-  # ✓ deleting from workbook body
-  # ✗ deleting from workbook sheets info
-  #
+  ## Delete sheet based on its name. Will ignore when it's cannot find the name.
+  ## Delete the first (or older) sheet when there's a same name.
+  ## Check `sheetNames proc<#sheetNames,Excel>`_ to get available names.
+
   # delete from workbook
   var rid = ""
   let ss = e.workbook.body.child "sheets"
@@ -309,20 +369,17 @@ proc deleteSheet*(e: Excel, name = "") =
     #inc nodepos
 
 
-proc deleteSheet*(w: Workbook, name = "") =
-  w.parent.deleteSheet name
-
 proc retrieveSheetsInfo(n: XmlNode): seq[XmlNode] =
   let sheets = n.child "sheets"
   if sheets == nil: return
   result = toSeq sheets.items
 
+# to add shared strings we need to
+# ✓ define the xml
+# ✓ add to content
+# ✓ add to package rels
+# ✓ update its path
 proc addSharedStrings(e: Excel) =
-  # to add shared strings we need to
-  # ✓ define the xml
-  # ✓ add to content
-  # ✓ add to package rels
-  # ✓ update its path
   e.sharedStrings[0] = (e.workbook.path.parentDir / "sharedStrings.xml").unixSep
   e.sharedStrings[1] = <>sst(xmlns=mainns, count="0", uniqueCount="0")
   e.content.add <>Override(PartName="/" & e.sharedStrings[0],
@@ -344,6 +401,10 @@ proc assignSheetInfo(e: Excel) =
 
 
 proc readExcel*(path: string): Excel =
+  ## Read Excel file from supplied path. Will raise OSError
+  ## in case path is not exists, IOError when system errors
+  ## during reading the file, ExcelError when the Excel file
+  ## is not valid (Excel file that has no workbook).
   let reader = openZipArchive path
   result = Excel()
   template extract(path: string): untyped =
@@ -389,6 +450,8 @@ proc readExcel*(path: string): Excel =
     s.sharedStrings = result.sharedStrings[1]
 
 proc addAppProp*(e: Excel, prop: varargs[(string, string)]) =
+  ## Add information property to Excel file. Will overwrite
+  ## any existing information property.
   const key = "app.xml"
   if key notin e.otherfiles: return
   let (_, propnode) = e.otherfiles[key]
@@ -396,10 +459,15 @@ proc addAppProp*(e: Excel, prop: varargs[(string, string)]) =
     propnode.add newXmlTree(p[0], [newText p[1]])
 
 proc newExcel*(appName = "Excelin"): (Excel, Sheet) =
+  ## Return a new Excel and Sheet at the same time to work for both.
+  ## The Sheet returned is by default has name "Sheet1" but user can
+  ## use `name= proc<#name=,Sheet>`_ to change its name.
   let excel = readExcel emptyxlsx
   (excel, excel.getSheet "Sheet1")
 
 proc writeFile*(e: Excel, targetpath: string) =
+  ## Write Excel to file in target path. Raise OSError when it can't
+  ## write to the intended path.
   let archive = ZipArchive()
   let lastmod = now().toTime
   template addContents(path, content: string) =
@@ -420,6 +488,9 @@ proc writeFile*(e: Excel, targetpath: string) =
   archive.writeZipArchive targetpath
 
 proc `$`*(e: Excel): string =
+  ## Get Excel file as string. Currently implemented by writing to
+  ## temporary dir first because there's no API to get the data
+  ## directly.
   let path = getTempDir() / fmt"excelin-{now().toTime.toUnix}.xml"
   e.writeFile path
   result = readFile path
@@ -429,10 +500,14 @@ proc `$`*(e: Excel): string =
     discard
 
 proc sheetNames*(e: Excel): seq[string] =
+  ## Return all availables sheet names within an Excel file.
   e.workbook.body.findAll("sheet").mapIt( it.attr "name" )
 
 proc name*(s: Sheet): string = s.privName
+  ## Get the name of current sheet
+
 proc `name=`*(s: Sheet, newname: string) =
+  ## Update sheet's name.
   s.privName = newname
   for node in s.parent.workbook.body.findAll "sheet":
     if s.rid == node.attr "r:id":
